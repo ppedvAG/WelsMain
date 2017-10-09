d89c83c1a58179ada10fec8c7304c74f9c5867ca
bill gates <hannesp@ppedv.de>
2017-10-09 14:58:10 +0200

    Excel Sheet öffnen

---
+++ WelsMain/WebForm8.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WelsMain
+{
+    public partial class WebForm8 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Webform9.aspx?id=Hannes");
+            //Response.RedirectPermanent()
+            Response.Write("Test");
+            Response.StatusCode = 404;
+           
+
+        }
+    }
+}
---
+++ WelsMain/WebForm9.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm9.aspx.cs" Inherits="WelsMain.WebForm9" %>
+
+
+        <table>
+            <tr>
+                <td>Daten</td>
+            </tr>
+            <tr>
+                <td>Daten1</td>
+            </tr>
+            <tr>
+                <td>Daten2</td>
+            </tr>
+            <tr>
+                <td>Daten3</td>
+            </tr>
+
+            <tr>
+                <td>Daten4</td>
+            </tr>
+        </table>
