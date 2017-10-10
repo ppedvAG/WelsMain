04fab0560a1a2b47f629592773f4a554d216b59e
bill gates <hannesp@ppedv.de>
2017-10-10 13:33:38 +0200

    Repeater Essen Liste + Edit

--- WelsMain/WebForm19.aspx.cs
+++ WelsMain/WebForm19.aspx.cs
@@ -14,26 +14,34 @@ namespace WelsMain
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            loadData();
+
+        }
+
+        private void loadData()
+        {
             var con = new SqlConnection(
-               ConfigurationManager.ConnectionStrings["Database1ConnectionString1"].ConnectionString);
+                           ConfigurationManager.ConnectionStrings["Database1ConnectionString1"].ConnectionString);
             var cmd = new SqlCommand("Select * from Essen", con);
             var da = new SqlDataAdapter(cmd);
             var ds = new DataTable();
             da.Fill(ds);
             rptEssen.DataSource = ds;
             rptEssen.DataBind();
-
         }
 
         protected void btnNeu_Click(object sender, EventArgs e)
         {
             var con = new SqlConnection(
               ConfigurationManager.ConnectionStrings["Database1ConnectionString1"].ConnectionString);
-            var cmd = new SqlCommand("Insert into Essen essen,preis values(@par1,@par2)", con);
+            var cmd = new SqlCommand("Insert into Essen (essen,preis) values(@par1,@par2)", con);
             cmd.Parameters.AddWithValue("@par1", txtessen.Text);
             cmd.Parameters.AddWithValue("@par2", txtpreis.Text);
+            con.Open();
             cmd.ExecuteNonQuery();
+            con.Close();
             txtessen.Text = txtpreis.Text = "";
+            loadData();
 
         }
     }
