6f77b1e66f6c908f044925f3fb69d7e3de9ae846
bill gates <hannesp@ppedv.de>
2017-10-10 08:42:11 +0200

    Textdatei anzeigen aus App_data

---
+++ WelsMain/WebForm12.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WelsMain
+{
+    public partial class WebForm12 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Request.QueryString.HasKeys())
+                {
+                    var qs = Request.QueryString["datei"];
+                    var vollHoller = Server.MapPath("~\app_data") + qs + ".txt";
+                    
+                    Literal1.Text=File.ReadAllText(vollHoller);
+
+                }
+            }
+            catch
+            { }
+        }
+    }
+}
