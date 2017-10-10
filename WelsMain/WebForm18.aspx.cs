d05841242e610b1af6f9acf1598860928e34bbf6
bill gates <hannesp@ppedv.de>
2017-10-10 12:06:03 +0200

    SQLDataadapter mit Datatable

---
+++ WelsMain/WebForm18.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WelsMain
+{
+    public partial class WebForm18 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var con = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["Database1ConnectionString1"].ConnectionString);
+            var cmd = new SqlCommand("Select * from Essen",con);
+            var da = new SqlDataAdapter(cmd);
+            var ds = new DataTable();
+            da.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+        }
+    }
+}
