33fedff6a9acca0e75665948b18a28636884e393
bill gates <hannesp@ppedv.de>
2017-10-11 13:30:55 +0200

    SQL Create Dummy Data

---
+++ dbo.Essen.data.sql
@@ -0,0 +1,10 @@
+﻿SET IDENTITY_INSERT [dbo].[Essen] ON
+INSERT INTO [dbo].[Essen] ([EssenId], [Essen], [Preis]) VALUES (1, N'Schweinsbraten', 5)
+INSERT INTO [dbo].[Essen] ([EssenId], [Essen], [Preis]) VALUES (2, N'Fisch', 3)
+INSERT INTO [dbo].[Essen] ([EssenId], [Essen], [Preis]) VALUES (3, N'Schnitzel', 6)
+INSERT INTO [dbo].[Essen] ([EssenId], [Essen], [Preis]) VALUES (4, N'Kaiserschmarrn', 4)
+INSERT INTO [dbo].[Essen] ([EssenId], [Essen], [Preis]) VALUES (5, N'Cola', 2)
+INSERT INTO [dbo].[Essen] ([EssenId], [Essen], [Preis]) VALUES (6, N'Bier', 2)
+INSERT INTO [dbo].[Essen] ([EssenId], [Essen], [Preis]) VALUES (7, N'Pommes', 2)
+INSERT INTO [dbo].[Essen] ([EssenId], [Essen], [Preis]) VALUES (8, N'Speckbohnen', 1)
+SET IDENTITY_INSERT [dbo].[Essen] OFF
---
+++ dbo.Verzehr.data.sql
@@ -0,0 +1,6 @@
+﻿SET IDENTITY_INSERT [dbo].[Verzehr] ON
+INSERT INTO [dbo].[Verzehr] ([VerzehrId], [PersonId], [EssenId], [Anzahl]) VALUES (1, 1, 1, 2)
+INSERT INTO [dbo].[Verzehr] ([VerzehrId], [PersonId], [EssenId], [Anzahl]) VALUES (2, 1, 3, 1)
+INSERT INTO [dbo].[Verzehr] ([VerzehrId], [PersonId], [EssenId], [Anzahl]) VALUES (3, 2, 1, 1)
+INSERT INTO [dbo].[Verzehr] ([VerzehrId], [PersonId], [EssenId], [Anzahl]) VALUES (4, 2, 4, 1)
+SET IDENTITY_INSERT [dbo].[Verzehr] OFF
