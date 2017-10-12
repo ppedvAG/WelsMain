3aa587f94e7866e958afc4188c42f55cb8771e86
bill gates <hannesp@ppedv.de>
2017-10-12 08:46:22 +0200

    Endlesspaging Ajax Gridview

---
+++ WelsMain/WebForm27.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm27.aspx.cs" Inherits="WelsMain.WebForm27" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <script src="Scripts/jquery-1.10.2.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" EnableViewState="false"
+                ItemType="WelsMain.Personen" SelectMethod="GridView1_GetData"
+                ></asp:GridView>
+            <a href="#" onclick="nachladen()">mehr....</a>
+        </div>
+    </form>
+    <script>
+        function nachladen()
+        {
+            $.ajax({
+                type: "GET",
+                url: "webform27/?p=" + page,
+                success: function (result) {
+                    var data = $(result).find("#GridView1 tbody");
+                    var rows = data.children();
+                    $("#GridView1 tbody").append(rows);
+                    page++;
+
+                }
+
+            });
+       
+        }
+        var page = 1;
+
+    </script>
+</body>
+</html>
---
+++ WelsMain/WebForm27.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WelsMain
+{
+    public partial class WebForm27 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView1.UseAccessibleHeader = true;
+            GridView1.HeaderRow.TableSection = TableRowSection.TableHeader;
+        }
+
+        // Der Rückgabetyp kann in 'IEnumerable' geändert werden. Damit Paginierung und Sortierung
+        // unterstützt werden, müssen jedoch die folgenden Parameter hinzugefügt werden:
+        //     int maximumRows
+        //     int startRowIndex
+        //     out int totalRowCount
+        //     string sortByExpression
+        public IQueryable<WelsMain.Personen> GridView1_GetData([QueryString("p")]int? page)
+        {
+            if (page==null)
+            {
+                page = 0;
+            }
+            var ef =new  Kantine();
+            var q = (from p in ef.Personen
+                     orderby p.PersonID
+                     select p).Skip(page.Value * 5).Take(5);
+            return q.AsQueryable();
+        }
+    }
+}
